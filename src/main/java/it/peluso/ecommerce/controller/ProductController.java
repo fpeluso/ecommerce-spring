0c676726dc4e81a7fd125df1947c64ac64937887
Fabio Peluso <peluso.f@gmail.com>
2021-03-25 10:57:39 +0100

    some tests

--- src/main/java/it/peluso/ecommerce/controller/ProductController.java
+++ src/main/java/it/peluso/ecommerce/controller/ProductController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import it.peluso.ecommerce.dto.ProductDTO;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
